0cc6da9643229ef2b91b5e50f4e9bdab76f55291
Aleksey <alekseytitov99@gmail.com>
2021-02-02 17:02:25 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/0_get_started_AlekseyTitov99.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0_get_started_AlekseyTitov99.exe"
+  "0_get_started_AlekseyTitov99.exe.manifest"
+  "0_get_started_AlekseyTitov99.lib"
+  "0_get_started_AlekseyTitov99.pdb"
+  "CMakeFiles/0_get_started_AlekseyTitov99.dir/helloworld.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0_get_started_AlekseyTitov99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
